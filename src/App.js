cb16e602141a86ec362428ee470ddb766971340c
Rafael Flores Pereira <rafael.flores@codeby.com.br>
2021-06-15 16:22:58 -0300

    created Cart Provider and Context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 /* React can be ommited in some new React projects */
-import { Fragment, useState } from "react";
+import { useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false)
@@ -17,13 +18,13 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onCloseCart={closeCartHandler}/>}
       <Header onClickCart={openCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
