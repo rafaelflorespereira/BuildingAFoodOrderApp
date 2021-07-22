6789e6e1a4695b467370162cd40982ca48ff4773
Rafael Flores Pereira <rafael.flores@codeby.com.br>
2021-07-22 16:17:32 -0300

    added cart effect through the useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,21 +1,44 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const [hasButtonEffect, setButtonEffect] = useState(false);
+  const { items } = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  /* Com || o hasButton da um lag bizarro */
+  const buttonHighlighted = `${classes.button} ${
+    hasButtonEffect ? classes.bump : ""
+  } `;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setButtonEffect(true);
+
+    const timer = setTimeout(() => {
+      setButtonEffect(false);
+    }, 300);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [items]);
+
   return (
-    <button onClick={props.onOpenClick}>
-      <span className={classes.button}>
+    <button className={buttonHighlighted} onClick={props.onOpenClick}>
+      <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span className={classes.icon}>Your Cart</span>
+      <span>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
